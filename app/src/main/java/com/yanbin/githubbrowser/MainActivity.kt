6c79e708086a23b62e73f037959f24a72769c510
YANBIN <yanbin.hung@eyesmedia.tw>
2020-10-10 22:24:25 +0800

    init commit

---
+++ app/src/main/java/com/yanbin/githubbrowser/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.yanbin.githubbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yanbin.githubbrowser.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
